b6478f30cd4a03f239cefe6d2321de431e93a8ff
jarek.jalocha@o2.pl <jarek.jalocha@o2.pl>
2023-01-09 18:56:35 +0100

    Update test_buttons test

--- zoom/test/test_buttons.js
+++ zoom/test/test_buttons.js
@@ -29,27 +29,3 @@ describe('Zoom website', () => {
         await expect(StartPage.buttonPhone).toHaveAttribute('href', 'tel:1.888.799.9666');
     })
 })
-
-
-
-    it('check demo button', async () => {
-        await StartPage.open();
-        await browser.maximizeWindow();
-        await LoginPage.buttonClose.click();
-        await StartPage.buttonReqADemo.click();
-        await expect(browser).toHaveUrl('https://explore.zoom.us/pl/livedemo/');
-        await browser.back();
-    })
-    it('check support button', async () => {
-        await StartPage.open();
-        await StartPage.buttonSupport.click();
-        await browser.switchWindow('Zoom Support')
-        await expect(browser).toHaveUrl('https://support.zoom.us/hc/en-us');
-        await browser.switchWindow('zoom.us')
-    })
-    it('check phone button', async () => {
-        await StartPage.buttonPhone.click();
-        await expect(browser).toHaveUrl('https://zoom.us/');
-        await expect(StartPage.buttonPhone).toHaveAttribute('href', 'tel:1.888.799.9666');
-    })
-})
